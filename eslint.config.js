880487151265be49dcb6f7d1909de5f4c125b2f4
AmarGupta-Shweta <105050993+AmarGupta-Shweta@users.noreply.github.com>
2025-12-12 16:16:42 +0530

    12dec packageUpdate

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+const { default: next } = require('next/eslint-plugin-next');
+
+module.exports = [
+  {
+    files: ['*/.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs['core-web-vitals'].rules,
+    },
+  },
+];
